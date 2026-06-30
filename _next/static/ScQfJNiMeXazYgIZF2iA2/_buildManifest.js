e18f28dae3dc63a59a20e377334852f85f643efa
Revanth <revanth@example.com>
2026-06-30 19:49:20 +0530

    Updates

---
+++ _next/static/ScQfJNiMeXazYgIZF2iA2/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/life-saver//_next/:path+",
+        "destination": "/life-saver/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
